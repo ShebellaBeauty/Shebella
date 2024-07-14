d3645575f25e4b81a808bc178ac7b741a2be140b
Yanguangchen <yanguangchen@outlook.com>
2024-07-14 12:15:41 +0800

    added quote.js

--- src/homepage.js
+++ src/homepage.js
@@ -5,11 +5,13 @@ import Listings from "./listings";
 import Contactparent from "./contactParent";
 import Socials from "./socials";
 import AboutSection from "./about";
+import Quote from "./quote";
 
 function Homepage() {
   return (
     <div>
       <SpliceElement />
+      <Quote />
       <AboutSection />
       <GridContainer />
       <Listings />
---
+++ src/quote.js
@@ -0,0 +1,27 @@
+import quoteImage from "./Assets/Quote.png";
+
+function Quote() {
+  const containerWrapper = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    textAlign: "center",
+    flexDirection: "column",
+    marginTop: "5%",
+    marginBottom: "3%",
+    overflow: "hidden",
+  };
+
+  const imageStyle = {
+    width: "40%",
+    borderRadius: "1.5rem",
+    border: "1px solid #e6a76c",
+  };
+
+  return <div style={containerWrapper}>
+    <img src={quoteImage} style={imageStyle} alt="quote" />
+  </div>;
+}
+
+export default Quote;
