23a6d9d6e89aca00a6327d0c1f42c85a70849c66
Chen yg <yanguangchen@outlook.com>
2024-07-27 11:55:56 +0800

    c

--- src/socials.js
+++ src/socials.js
@@ -11,6 +11,7 @@ function Socials() {
       </div>
       <ElfsightWidget widgetId="ef870bda-cf78-488e-85b5-109115b61d29" />
       <ElfsightWidget widgetId="79f39766-0869-4581-8627-665d8ce21eba" />
+      <iframe src='https://widgets.sociablekit.com/medium-publication-feed/iframe/25442466' style={{frameborder:'0',width:'95%',height:'1000px'}}></iframe>
     </div>
   );
 }
